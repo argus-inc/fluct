a81521ac6cb78b3d4a8e395f7be78af7a28d7262
Mederic Burlet <mederic.burlet@pixiumdigital.com>
2020-06-23 02:55:34 +0800

    :sparkles: feat(doc): Adding documentation

--- src/FileHandler.ts
+++ src/FileHandler.ts
@@ -2,28 +2,61 @@ import * as os from 'os'
 import * as path from 'path'
 import * as fs from 'fs'
 
-export const configDirectory = ".dust"
-
-export const configDirectoryPath = path.join(os.homedir(), configDirectory)
-
+/**
+ * Returns an instance of the FileHandler class.
+ *
+ * @param tempDirectoryName - The name of the temp directory
+ * @returns `FileHandler`  - The initiated class of file handler
+ *
+ */
 export class FileHandler {
-    
-    constructor() {
+    /**
+     * Holds the temp directory name
+     */ 
+    public tempDirectoryName: string;
+    /**
+     * Holds the temp directory path
+     */ 
+    public tempDirectoryPath: string;
+
+    constructor(tempDirectoryName = ".fluct") {
+        this.tempDirectoryName = tempDirectoryName
+        this.tempDirectoryPath = path.join(os.homedir(), this.tempDirectoryName)
         this.createTempDir()
     }
 
+    /**
+     * Returns if the temp directory exists.
+     *
+     * @returns `boolean`  - True if the folder exists
+     *
+     */
     doesTempDirExist = ():boolean => {
-        const pathed = path.join(configDirectoryPath);
+        const pathed = path.join(this.tempDirectoryPath);
         if (fs.existsSync(pathed)) {
             return true
         }
         return false
     }
 
+    /**
+     * Returns if the temp directory is writable.
+     *
+     * @returns `boolean`  - True if the folder is writable
+     *
+     */
     isTempDirWritable = (): boolean => {
-        return this.isDirWritable(configDirectoryPath)
+        return this.isDirWritable(this.tempDirectoryPath)
     }
 
+    /**
+     * Returns if the given directory is writable.
+     *
+     * @param path - A `string` of the path to the directory
+     * 
+     * @returns `boolean`  - True if the folder is writable
+     *
+     */
     isDirWritable = (path: string): boolean => {
         try {
             fs.accessSync(path, fs.constants.W_OK);
@@ -34,10 +67,24 @@ export class FileHandler {
         }
     }
 
+    /**
+     * Creates the temp directory
+     *
+     * @returns `boolean`  - True if the folder was created
+     *
+     */
     createTempDir = (): boolean => {
-        return this.createDir(configDirectoryPath);
+        return this.createDir(this.tempDirectoryPath);
     }
 
+    /**
+     * Creates an empty file at the given path
+     *
+     * @param path - A `string` of the path to the file
+     * 
+     * @returns `boolean`  - True if the file was created
+     *
+     */
     touch = (path: string) => {
         if (this.exists(path)) {
             return true;
@@ -51,14 +98,40 @@ export class FileHandler {
         }
     }
 
+    /**
+     * Deletes a file at the given path
+     *
+     * @param path - A `string` of the path to the file
+     *
+     */
     delete = (path: string) => {
         if (this.exists(path)) {
             fs.unlinkSync(path)
         }
     } 
 
-    read = (content: string) => fs.readFileSync(content,{encoding:'utf8', flag:'r'})
+    /**
+     * Reads the content of a file in sync
+     *
+     * @param path - A `string` of the path to the file
+     * 
+     * @returns `string`  - The content in utf8 format
+     * 
+     * @remark TODO: add encoding customization
+     *
+     */
+    read = (path: string) => fs.readFileSync(path,{encoding:'utf8', flag:'r'})
 
+    /**
+     * Saves content to a file at a given path in sync
+     * 
+     * @remark `objects` are automatically saved as JSON
+     *
+     * @param path - A `string` of the path to the file
+     * 
+     * @returns `boolean`  - True if the file was saved with desired content
+     *
+     */
     save = (path: string, content: any) => {
         let formatedContent = ""
         switch (typeof content) {
@@ -78,10 +151,26 @@ export class FileHandler {
         }
     }
 
+    /**
+     * Checks if the given path exists in sync
+     *
+     * @param path - A `string` of the path to check
+     * 
+     * @returns `boolean`  - True if the given path exists
+     *
+     */
     exists = (path: string) => {
         return fs.existsSync(path)
     } 
 
+    /**
+     * Creates a directpry at the given path in sync
+     *
+     * @param path - A `string` of the path to the directory
+     * 
+     * @returns `boolean`  - True if the directory was created
+     *
+     */
     createDir = (path: string): boolean => {
         if (this.exists(path) === false) {
             fs.mkdirSync(path)
@@ -93,15 +182,31 @@ export class FileHandler {
         return true
     }
 
-    create_path = (paths: string[], fromAppTempDir: boolean = true, fromRunningDir: boolean = false):string => {
+    /**
+     * Builds proper paths
+     *
+     * @param paths - A `string[]` of the path to merge file
+     * 
+     * @param fromAppTempDir - A `boolean` to know if the said path should be from the temp directory
+     * 
+     * @param fromRunningDir - A `boolean` to know if the said path should be from the running directory
+     * 
+     * @returns `string`  - True if the file was created
+     * 
+     * @example 
+     * Here's an example with `fromAppTempDir`:
+     * ```
+     * // returns `~/.fluct/mypath/myFile.md`
+     * createPath([`mypath`, `myFile.md`], true)
+     * ```
+     */
+    createPath = (paths: string[], fromAppTempDir: boolean = true, fromRunningDir: boolean = false):string => {
         if (fromAppTempDir === true) {
-            return path.join(configDirectoryPath, ...paths)
+            return path.join(this.tempDirectoryPath, ...paths)
         } else if (fromRunningDir) {
             return path.join(__dirname, ...paths)
         } else {
             return path.join(...paths)
         }
     }
-
-    
 }
